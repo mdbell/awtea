5e1c4954d936795a511a600e89cb572bd13f745d
Matt <github@mdbell.me>
2025-12-02 18:01:18 -0500

    Rename DeterministicRandom to Random and update methods for better compatibility with Java streams

--- src/main/java/me/mdbell/awtea/util/ColorUtil.java
+++ src/main/java/me/mdbell/awtea/util/ColorUtil.java
@@ -9,28 +9,4 @@ public class ColorUtil {
     public static String toCSS(Color color) {
         return "rgba(" + color.getRed() + "," + color.getGreen() + "," + color.getBlue() + "," + color.getAlpha() + ")";
     }
-
-    public static int toRGBA(Color color) {
-        return (color.getRed() << 24) | (color.getGreen() << 16) | (color.getBlue() << 8) | color.getAlpha();
-    }
-
-    public static void normalize(Color color, Float32Array arr) {
-        arr.set(0, color.getRed() / 255f);
-        arr.set(1, color.getGreen() / 255f);
-        arr.set(2, color.getBlue() / 255f);
-        arr.set(3, color.getAlpha() / 255f);
-    }
-
-    public static void normalize(Color color, float[] arr) {
-        arr[0] = color.getRed() / 255f;
-        arr[1] = color.getGreen() / 255f;
-        arr[2] = color.getBlue() / 255f;
-        arr[3] = color.getAlpha() / 255f;
-    }
-
-    public static float[] normalize(Color color) {
-        float[] arr = new float[4];
-        normalize(color, arr);
-        return arr;
-    }
 }
--- src/main/java/me/mdbell/awtea/util/jso/Atlas.java
+++
@@ -1,9 +0,0 @@
-package me.mdbell.awtea.util.jso;
-
-import org.teavm.jso.JSIndexer;
-import org.teavm.jso.JSObject;
-
-public interface Atlas extends JSObject {
-    @JSIndexer
-    Glyph get(String key);
-}
