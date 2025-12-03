27b5002dbbe4fd38de76b324f6b3eefe7167611f
Matt <github@mdbell.me>
2025-12-02 19:40:46 -0500

    rename MidiResourceSupplier to SoundResourceSupplier

--- src/main/java/me/mdbell/awtea/transform/SoundResourceSupplier.java
+++ src/main/java/me/mdbell/awtea/transform/SoundResourceSupplier.java
@@ -3,7 +3,7 @@ package me.mdbell.awtea.transform;
 import org.teavm.classlib.ResourceSupplier;
 import org.teavm.classlib.ResourceSupplierContext;
 
-public class MidiResourceSupplier implements ResourceSupplier {
+public class SoundResourceSupplier implements ResourceSupplier {
     @Override
     public String[] supplyResources(ResourceSupplierContext context) {
         return new String[]{
