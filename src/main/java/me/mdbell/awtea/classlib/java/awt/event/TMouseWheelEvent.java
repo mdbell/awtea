2ed39a6e3f4494c15e0931d0e909a03d24819100
Matt <github@mdbell.me>
2025-12-07 18:08:12 -0500

    feat(mouse): refactor TMouseWheelEvent and TMouseWheelListener for improved event handling

--- src/main/java/me/mdbell/awtea/classlib/java/awt/event/TMouseWheelEvent.java
+++ src/main/java/me/mdbell/awtea/classlib/java/awt/event/TMouseWheelEvent.java
@@ -2,11 +2,11 @@ package me.mdbell.awtea.classlib.java.awt.event;
 
 import lombok.Getter;
 import lombok.ToString;
-import org.teavm.jso.dom.html.HTMLCanvasElement;
 import me.mdbell.awtea.classlib.java.awt.TComponent;
 import me.mdbell.awtea.input.MouseButtonType;
 import me.mdbell.awtea.input.MouseEventType;
 import me.mdbell.awtea.util.Point;
+import org.teavm.jso.dom.html.HTMLCanvasElement;
 
 @ToString(callSuper = true)
 @Getter
@@ -15,42 +15,43 @@ import me.mdbell.awtea.util.Point;
  */
 public class TMouseWheelEvent extends TMouseEvent {
 
-    public static final int SCROLL_AMOUNT = 3;
+	public static final int SCROLL_AMOUNT = 3;
 
-    private final double preciseWheelRotation;
-    private final int scrollAmount;
-    private final int scrollType;
-    private final int unitsToScroll;
-    private final int wheelRotation;
+	private final double preciseWheelRotation;
+	private final int scrollAmount;
+	private final int scrollType;
+	private final int unitsToScroll;
+	private final int wheelRotation;
 
-    public TMouseWheelEvent(TComponent component, int id, int x, int y, MouseButtonType button, boolean meta,
-                            double preciseWheelRotation, int scrollAmount, int scrollType, int unitsToScroll, int wheelRotation) {
-        super(component, id, x, y, button, meta);
-        this.preciseWheelRotation = preciseWheelRotation;
-        this.scrollAmount = scrollAmount;
-        this.scrollType = scrollType;
-        this.unitsToScroll = unitsToScroll;
-        this.wheelRotation = wheelRotation;
-    }
+	public TMouseWheelEvent(TComponent component, int id, int x, int y, MouseButtonType button, boolean meta,
+							double preciseWheelRotation, int scrollAmount, int scrollType, int unitsToScroll, int wheelRotation) {
+		super(component, id, x, y, button, meta);
+		this.preciseWheelRotation = preciseWheelRotation;
+		this.scrollAmount = scrollAmount;
+		this.scrollType = scrollType;
+		this.unitsToScroll = unitsToScroll;
+		this.wheelRotation = wheelRotation;
+	}
 
-    public String paramString() {
-        return null;
-    }
+	public String paramString() {
+		return null;
+	}
 
-    public static TMouseWheelEvent adapt(TComponent component, HTMLCanvasElement src, org.teavm.jso.dom.events.WheelEvent event, String type) {
-        MouseEventType source = MouseEventType.fromHtml(type);
+	public static TMouseWheelEvent adapt(TComponent component, HTMLCanvasElement src,
+										 org.teavm.jso.dom.events.WheelEvent event, String type) {
+		MouseEventType source = MouseEventType.fromHtml(type);
 
-        Point point = new Point(event.getClientX(), event.getClientY());
+		Point point = new Point(event.getClientX(), event.getClientY());
 
-        translatePoint(point, src);
+		translatePoint(point, src);
 
-        double deltaY = event.getDeltaY();
-        int scollType = event.getDeltaMode(); // TODO: verify the values map 1-1
-        int wheelRotation = (int) Math.signum(deltaY);
-        int unitsToScroll = wheelRotation * SCROLL_AMOUNT;
+		double deltaY = event.getDeltaY();
+		int scollType = event.getDeltaMode(); // TODO: verify the values map 1-1
+		int wheelRotation = (int) Math.signum(deltaY);
+		int unitsToScroll = wheelRotation * SCROLL_AMOUNT;
 
-        MouseButtonType button = MouseButtonType.fromHtml(event.getButton());
-        return new TMouseWheelEvent(component, source.getId(), point.getX(), point.getY(),
-                button, event.getMetaKey(), deltaY, SCROLL_AMOUNT, scollType, unitsToScroll, wheelRotation);
-    }
+		MouseButtonType button = MouseButtonType.fromHtml(event.getButton());
+		return new TMouseWheelEvent(component, source.getId(), point.getX(), point.getY(),
+			button, event.getMetaKey(), deltaY, SCROLL_AMOUNT, scollType, unitsToScroll, wheelRotation);
+	}
 }
--- src/main/java/me/mdbell/awtea/classlib/java/awt/event/TMouseWheelListener.java
+++ src/main/java/me/mdbell/awtea/classlib/java/awt/event/TMouseWheelListener.java
@@ -1,9 +1,11 @@
 package me.mdbell.awtea.classlib.java.awt.event;
 
+import org.teavm.classlib.java.util.TEventListener;
+
 /**
  * @see java.awt.event.MouseWheelListener
  */
-public interface TMouseWheelListener {
+public interface TMouseWheelListener extends TEventListener {
 
-    void mouseWheelMoved(TMouseWheelEvent event);
+	void mouseWheelMoved(TMouseWheelEvent event);
 }
