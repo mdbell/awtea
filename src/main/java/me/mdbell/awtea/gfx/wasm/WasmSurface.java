a87f66a28fa480a5b4381f6961855b9fa205b061
Matt <github@mdbell.me>
2025-12-11 23:22:32 -0500

    more cleanup

--- src/main/java/me/mdbell/awtea/gfx/wasm/WasmSurface.java
+++ src/main/java/me/mdbell/awtea/gfx/wasm/WasmSurface.java
@@ -3,9 +3,7 @@ package me.mdbell.awtea.gfx.wasm;
 import lombok.Getter;
 import me.mdbell.awtea.gfx.Rasterizer;
 import me.mdbell.awtea.gfx.Surface;
-import org.teavm.jso.canvas.ImageData;
 import org.teavm.jso.typedarrays.ArrayBuffer;
-import org.teavm.jso.typedarrays.Int32Array;
 import org.teavm.jso.typedarrays.Uint8ClampedArray;
 
 public final class WasmSurface implements Surface {
@@ -106,34 +104,7 @@ public final class WasmSurface implements Surface {
 		return pixelFormat;
 	}
 
-	public Int32Array getPixelsView() {
-
-		if (surfaceId == -1) {
-			throw new IllegalStateException("Surface has been destroyed");
-		}
-
-		int ptr = getPixelsPtr();
-		int stride = getStride();
-		int height = getHeight();
-
-		int lengthInInts = (stride / 4) * height;
-
-		// pointer is in 32-bit “bytes” space, so >> 2 to get Int32Array index
-		return new Int32Array(memoryBuffer, ptr, lengthInInts);
-	}
-
-	public ImageData asImageData() {
-		int width = getWidth();
-		int height = getHeight();
-		int stride = getStride();
-		int pixelsPtr = getPixelsPtr();
-
-		// Create ImageData
-		Uint8ClampedArray wasmPixels = new Uint8ClampedArray(memoryBuffer, pixelsPtr, stride * height);
-
-		return new ImageData(wasmPixels, width, height);
-	}
-
+	@Override
 	public void destroy() {
 		if (surfaceId != -1) {
 			// surfaces with a width & height of 0 are considered free
